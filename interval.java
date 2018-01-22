0f11c3e2a4f87faca5e89fdfea029150f2d0bcf0
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-01-22 16:25:20 +0530

    java program
    even numbers between two intervals

---
+++ interval.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ListEvennumbers
+{
+public static void main (string[]args)
+{
+int limit=50;
+system.out.print\n("printing even numbers between 1 and +"limit);
+for(int i=1;i<=limit;i++)
+{
+if(i%2==0)
+{
+system.out.print(i+"");
+}
+}
+}
+}
