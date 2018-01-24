2e03978af54f381435d070e624cb0ebdb32addee
SOWMIYA21 <35294707+SOWMIYA21@users.noreply.github.com>
2018-01-24 16:22:22 +0530

    java program
    number N and array of an N integer

---
+++ maximum.java
@@ -0,0 +1,20 @@
+import java.util.scanner;
+class sum
+{
+public static void main(string args[])
+{
+scanner scanner=new scanner(system.in);
+int array=new int;
+int sum=0
+system.out.println("enter the element:");
+for(int i=n;i<n;i++)
+{
+array[i]=scanner.nextInt();
+}
+for(int num:array)
+{
+sum=sum+num;
+}
+system.out.println("sum of array element is:"+sum);
+}
+}
